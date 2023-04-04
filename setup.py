5277eb585ec326cbfba66b641b1e0a0ee8dac8bf
yangsenius <465097485@qq.com>
2023-04-04 16:41:34 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="yolov7-object-tracking",
+    version="0.1",
+    author="yangsenius",
+    description="yolo-tracker",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
